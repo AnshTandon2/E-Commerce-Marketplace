d0b3a12e9426ad4c67f076538a6d4fed8ad403a1
Lalitha Chandolu <143627196+lalitha-chandolu@users.noreply.github.com>
2023-11-11 17:20:40 -0500

    Updated user class to verify login information

--- User.java
+++ User.java
@@ -2,22 +2,31 @@ import java.util.*;
 import java.time.*;
 
 /**
- * Program Name
+ * User Class
  * <p>
- * Brief description of program
+ * Initiates a user object
+ * Parent class of Buyer and Seller
+ * Includes private fields including user email and password
  *
- * @author Ansh Tandon, CS 180 Black
- * @version Date of Completion
+ * @author Ansh Tandon, Lalitha Chandolu; CS 180 Black
+ * @version November 11, 2023
  */
 
 
 public class User {
-    String name;
-    String email;
-    String password;
-    public User(String name, String email) {
+    private String name;
+    private String email;
+    private int userId;
+    private String password;
+
+    public static int userCounter = 1;
+
+    public User(String name, String email, String password) {
         this.name = name;
         this.email = email;
+        this.password = password;
+        this.userId = userCounter;
+        userCounter++;
     }
 
     public String getName() {
@@ -27,4 +36,24 @@ public class User {
     public String getEmail() {
         return this.email;
     }
+
+    public String getPassword() { 
+        return this.password; 
+    }
+
+    public int getUserId() { 
+        return this.userId; 
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }    
+
+    public void setPassword(String password) {
+        this.password = password;
+    }    
+
+    public boolean accountExists(String email, String password) {
+        return (this.email.equals(email) && this.password.equals(password));
+    }    
 }
