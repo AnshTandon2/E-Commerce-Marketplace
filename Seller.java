c8593175af22935b8350b127f0cd27dbfb34674c
Lalitha Chandolu <143627196+lalitha-chandolu@users.noreply.github.com>
2023-11-12 12:39:33 -0500

    Updated Seller.java

--- Seller.java
+++ Seller.java
@@ -5,17 +5,8 @@ public class Seller extends User{
     // email
     // name
     // password
-
-<<<<<<< HEAD
-//    private PurchaseHistory purchaseHistory;
-    private ShoppingCart shoppingCart;
-    //store shopping cart and purchase history in separate clases and hten reference usernames in that file
-    //buyer info and seller info (one csv file) 
-    //
-    public Seller(String info, boolean hasStores) {
-=======
+    
     public Seller(String info, ArrayList<Stores> storeList) {
->>>>>>> 5deea63ed7457c2117f360e2f3b2451f0f2a4e3a
         String[] arr = info.split(","); //Ansh,tandon39@purdue.du,hello
         // calls the parent constructor and sends the seller's name, email, and password
         super(arr[0], arr[1], arr[2]);
