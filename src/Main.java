24a61d02125243f2859da9da92a6b9a374a5fbc8
BOT Waldo <107410510+LucaLuca448@users.noreply.github.com>
2023-11-12 16:47:15 -0500

    Added basic functionality for login and logout

--- src/Main.java
+++ src/Main.java
@@ -17,42 +17,111 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello and welcome!");
 
+        String userName = "";
+        String password = "";
+        String email = "";
 
-        boolean valid = false;
-        int pg1Option = 0;
+        boolean loggedIn = false;
         do {
             try {
                 System.out.println("Select an option:\n1. Login\n2. Create New Account");
                 String firstPgOption = scan.nextLine();
-                if (Integer.parseInt(firstPgOption) == 1 || Integer.parseInt(firstPgOption) == 2) {
-                    valid = true;
-                    pg1Option = Integer.parseInt(firstPgOption);
+                if (Integer.parseInt(firstPgOption) == 1) {
+                    System.out.println("Please Enter your Username");
+                    String un = scan.nextLine();
+                    System.out.println("Please enter your password"); 
+                    String pw = scan.nextLine();
+                    User newUser = new User(un, "placeHolder", pw);
+                    if (!newUser.checkAccountExists()) {
+                        System.out.println("Username or password incorrect");
+                    } else {
+                        userName = un;
+                        password = pw;
+                        loggedIn = true;
+                    }
+                } else if (Integer.parseInt(firstPgOption) == 2) {
+                    System.out.println("Please enter your new Username");
+                    String newUN = scan.nextLine();
+                    System.out.println("Please enter your new password");
+                    String newPW = scan.nextLine();
+                    System.out.println("Please enter your new email");
+                    String newEmail = scan.nextLine();
+                    System.out.println("Would you like to be a buyer (enter 1) or seller (enter 2)?");
+                    String sellerBuyer = scan.nextLine();
+                    User registeringUser = new User(newUN, newEmail, newPW);
+                    if (registeringUser.checkAccountExists()) {
+                        System.out.println("Account already exists");
+                    } else {
+                        if (Integer.parseInt(sellerBuyer) == 1) {
+                            registeringUser.makeNewUser(newUN, newPW, newEmail, true);
+                        } else if (Integer.parseInt(sellerBuyer) == 2) {
+                            registeringUser.makeNewUser(newUN, newPW, newEmail, false);
+                        }
+                        //loggedIn = true;
+                    }
                 }
             } catch (NumberFormatException e) {
                 System.out.println("Please enter a valid choice");
             }
-        } while (!valid);
+        } while (!loggedIn);
+        
+        boolean loggedOut = false;
+        User user = new User(userName, email, password);
+        while (!loggedOut) {
 
-        if (pg1Option == 1) {
-            System.out.println("Please Enter your Username");
-            String un = scan.nextLine();
-            System.out.println("Please enter your password"); 
-            String pw = scan.nextLine();
-            User newUser = new User(un, "placeHolder", pw);
-            if (!newUser.checkAccountExists()) {
-                System.out.println("Username or password incorrect");
+            if (user.isBuyer(userName, password)) {
+                System.out.println("Main Menu Options:");
+                System.out.println("1. Search for items");
+                System.out.println("2. View Purchase history");
+                System.out.println("3. View Statistics Dashboard");
+                System.out.println("4. Logout");
+                String mainMenuOption = scan.nextLine();
+
+                if (mainMenuOption.equals("1")) {
+
+                } else if (mainMenuOption.equals("2")) {
+
+                } else if (mainMenuOption.equals("3")) {
+
+                } else if (mainMenuOption.equals("4")) {
+                    loggedOut = true;
+                } else {
+                    System.out.println("Please enter a valid choice!");
+                }
+                
+
+            } else {
+                System.out.println("Main Menu Options:");
+                System.out.println("1. Manage my stores");
+                System.out.println("2. View Selling history of stores");
+                System.out.println("3. View Statistics Dashboard");
+                System.out.println("4. Logout");
+                String mainMenuOption = scan.nextLine();
+
+                if (mainMenuOption.equals("1")) {
+
+                } else if (mainMenuOption.equals("2")) {
+
+                } else if (mainMenuOption.equals("3")) {
+
+                } else if (mainMenuOption.equals("4")) {
+                    loggedOut = true;
+                } else {
+                    System.out.println("Please enter a valid choice!");
+                }
+                
             }
         }
-        
+
+        System.out.println("Thank you for visiting! You are now logged out");
 
         
-
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        /*for (int i = 1; i <= 5; i++) {
 
             // Press Ctrl+D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
-        }
+        } */
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -60,6 +61,61 @@ public class User {
             e.printStackTrace();
         }
         return returnOption;
+    }
+
+    public boolean isBuyer(String name, String password) { //only call this method if user is verified to exist/registered
+        File f = new File("users.txt");
+        boolean buyerStatus = true;
+        try {
+            Scanner scan = new Scanner(f);
+            while (scan.hasNextLine()) {
+                String[] data = scan.nextLine().split(",");
+                if (data[1].equals(name) && data[2].equals(password)) {
+                    if (data[3].equals("s")) {
+                        buyerStatus = false;
+                    }
+                }
+
+            }
+            scan.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return buyerStatus;
+    }
+
+    public void makeNewUser(String name, String password, String email, boolean buyer) {
+        File f = new File("users.txt");
+        try {
+            FileWriter fw = new FileWriter(f);
+            if (buyer) {
+                fw.write("\n");
+                fw.write(email + "," + name + "," + password + ",b");
+                File f2 = new File("/src/Buyers/" + name +".txt");
+                f2.createNewFile();
+                FileWriter fw2 = new FileWriter(f2);
+                fw2.write(name);
+                fw2.write("\n");
+                fw2.write(password);
+                fw2.write("\n");
+                fw2.close();
+            } else {
+                fw.write("\n");
+                fw.write(email + "," + name + "," + password + ",s");
+                File f3 = new File("/src/Sellers/" + name + ".txt");
+                f3.createNewFile();
+                FileWriter fw3 = new FileWriter(f3);
+                fw3.write(name);
+                fw3.write("\n");
+                fw3.write(password);
+                fw3.write("\n");
+                fw3.close();
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("A fatal error has occured");
+        }
     }
 
 
