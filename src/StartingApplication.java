b46a8a7436f287175ecd47432d95fb1f5ffe209d
Lalitha Chandolu <143627196+lalitha-chandolu@users.noreply.github.com>
2023-11-13 11:49:59 -0500

    Update StartingApplication.java

--- src/StartingApplication.java
+++ src/StartingApplication.java
@@ -1,45 +1,243 @@
-/**
- * Seller Class
- * <p>
- * Initializes a seller object and
- * manages their selling history and identification
- * information.
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+/** Starting Application Class
+ * Serves as the Login/Sign up system for a user
+ * Verifies that they use a @purdue.edu email and
+ * whether they are a buyer or seller
+ * The class redirects them to the corresponding menu
  *
- * @author Nirmal Senthilkumar; Ansh Tandon CS 180 Black
+ * @author Lalitha Chandolu, Ansh Tandon, Justin Ho-Yuk
  * @version November 12, 2023
+ *
  */
-public class Seller {
-    private String name;
-    private String email;
-    private String password;
 
-    public Seller(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
+public class StartingApplication {
+
+    // public static AccountManager am = new AccountManager();
+
+    public static void main(String[] args) {
+        // g2g
+        System.out.println("Welcome to the Boilermaker Bazaar! ");
+        Scanner s = new Scanner(System.in);
+        // welcomes the user to the application
+        // redirects them to sign up or sign in page
+        boolean redirected = false;
+        boolean exitOnFirst = false;
+        String username = "";
+        String password = "";
+        String userRole = "";
+        do {
+            try {
+                int value = 0;
+                System.out.println("Please select one of the three options: " +
+                        "\n1. Login to Application" +
+                        "\n2. Sign Up with a New Account" +
+                        "\n3. Exit");
+                value = Integer.parseInt(s.nextLine());
+                if (value == 1) {
+                    //  the user has an existing account and would like to login to the marketplace
+                    System.out.println("Enter username");
+                    username = s.nextLine();
+                    System.out.println("Enter password");
+                    password = s.nextLine();
+                    // verifies that the existing account exists
+                    userRole = accountExists(username, password);
+                    if (userRole == null) {
+                        // user inputted wrong information or user doesn't currently exist
+                        System.out.println("The username or password is incorrect");
+                        // should go back again to main menu due to error
+                    } else {
+                        // signed in successfully
+                        System.out.println("Success, logging you in now");
+                        redirected = true;
+                    }
+                } else if (value == 2) {
+                    // would like to make a new account in the marketplace
+                    System.out.println("Enter your name");
+                    String name = s.nextLine().toLowerCase();
+                    // allows each user to have a unique identifier
+                    System.out.println("Enter your Purdue username (the part before your @purdue.edu)");
+                    username = s.nextLine();
+                    System.out.println("Enter new password");
+                    password = s.nextLine();
+                    System.out.println("Are you signing up as a:\n [1] Seller\n[2] Buyer");
+                    String roleChoice = s.nextLine();
+                    // checks if they have an existing account or not
+                    userRole = accountExists(username, password);
+                    // they don't have an existing account - so they can make an account
+                    if (userRole == null) {
+                        // they want to have the role of a seller
+                        File f = new File("/data/users.txt");
+                        try {
+                            FileWriter fw = new FileWriter(f, true);
+                            BufferedWriter bfw = new BufferedWriter(fw);
+                            // populates a list of sellers with the username (purdue email)
+                            if (roleChoice.equals("1")) {
+                                // s is indicator for seller user type
+                                bfw.write(username + ";" + password + ";" + name + ";" + "s");
+                            } else if (roleChoice.equals("2")) {
+                                // c is indicator for customer user type
+                                bfw.write(username + ";" + password + ";" + name + ";" + "c");
+                            } else {
+                                System.out.println("Please try again!");
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        // account exists - go back to main menu
+                        System.out.println("Account already exists");
+                    }
+                } else if (value == 3) {
+                    // would like to exit the application
+                    System.out.println("Thank you for using Purdue Bazaar!");
+                    redirected = true;
+                    exitOnFirst = true;
+                } else {
+                    // invalid choice was enetered
+                    System.out.println("Please try again. Make sure you enter a valid choice.");
+                }
+            } catch (NumberFormatException e) {
+                // user didn't enter a number
+                System.out.println("There was an error in your input, please try again");
+            }
+        } while (!redirected);
+
+
+        boolean loggedOut = exitOnFirst;
+        Marketplace.initializeMarketplace();
+        while (!loggedOut) {
+            if (userRole.equalsIgnoreCase("seller")) {
+                System.out.println("Seller Main Menu:\n1. View Marketplace\n2. View all sales by store\n3. Add a Product\n" +
+                        "4. Edit a Product\n5. Delete a Product\n6. View Store Statistics\n7. Log Out");
+                String MMChoice = s.nextLine();
+                if (MMChoice.equals("1")) {
+                    Marketplace.printMarketplace();
+                } else if (MMChoice.equals("2")) {
+                    System.out.println();
+                } else if (MMChoice.equals("3")) {
+
+                } else if (MMChoice.equals("4")) {
+
+                } else if (MMChoice.equals("5")) {
+
+                } else if (MMChoice.equals("6")) {
+
+                } else if (MMChoice.equals("7")) {
+                    loggedOut = true;
+                } else {
+                    System.out.println("Please try again with valid input!");
+                }
+
+
+            } else {
+                // the user is a Customer type
+                System.out.println("Customer Main Menu:\n1. View Marketplace\n2. View Shopping Cart\n3. Search for Product\n" +
+                        "4. View Shopping History\n5. Log Out");
+                String MMChoice = s.nextLine();
+                if (MMChoice.equals("1")) {
+                    Marketplace.printMarketplace();
+                    System.out.println("1. Sort by Ascending Price\n2. Sort by Descending price\n3. Sort by ascending quantity\n4. Sort by Descending Quantity\n(Anything else.) exit");
+                    String sortChoice = s.nextLine();
+                    if (sortChoice.equals("1")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket(null, "price", "asc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else if (sortChoice.equals("2")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket(null, "price", "dsc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else if (sortChoice.equals("3")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket(null, "quantity", "asc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else if (sortChoice.equals("4")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket(null, "quantity", "dsc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else {
+                        System.out.println("try again");
+                    }
+                } else if (MMChoice.equals("2")) {
+                    ArrayList<String> shoppingCart = Marketplace.displayShoppingCart(username);
+                    for (String str : shoppingCart) {
+                        System.out.println(str + "\n");
+                    }
+                } else if (MMChoice.equals("3")) {
+                    System.out.println("Enter your search term: ");
+                    String keyword = s.nextLine();
+                    ArrayList<String> preReq = new ArrayList<>();
+                    for (String str : Marketplace.searchProduct(keyword)) {
+                        System.out.println(str);
+                        preReq.add(str.split(";")[0]);
+                    }
+                    System.out.println("1. Sort by Ascending Price\n2. Sort by Descending price\n3. Sort by ascending quantity\n4. Sort by Descending Quantity\n(Anything else.) exit");
+                    String sortChoice = s.nextLine();
+
+
+
+                    if (sortChoice.equals("1")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket("price", "asc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else if (sortChoice.equals("2")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket("price", "dsc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else if (sortChoice.equals("3")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket("quantity", "asc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else if (sortChoice.equals("4")) {
+                        ArrayList<String> sortedIndexes = Marketplace.sortMarket("quantity", "dsc");
+                        for (String str : sortedIndexes) {
+                            System.out.println(Marketplace.getProductInfo(Integer.parseInt(str)));
+                        }
+                    } else {
+                        System.out.println("try again");
+                    }
+                } else if (MMChoice.equals("4")) {
+
+                } else if (MMChoice.equals("5")) {
+                    loggedOut = true;
+                } else {
+                    System.out.println("Please try again with valid input!");
+                }
+
+            }
+        }
+    }
+    public static String accountExists (String username, String password) {
+        // parses the file of all of the existing user in the marketplace
+        File f = new File("/data/users.txt");
+        try {
+            FileReader fr = new FileReader(f);
+            BufferedReader bfr = new BufferedReader(fr);
+            String line = bfr.readLine();
+            while (line != null) {
+                // splits the line by the ";" token
+                String[] temp = line.split(";");
+                // email and password given match
+                if (temp[0].equals(username) && temp[1].equals(password)) {
+                    // returns the userRole (s for Seller, c for Customer)
+                    return temp[3];
+                }
+                line = bfr.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // the info given doesn't match with an existing user
+        return null;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
