d4e0b22a652ca6e36e2e803c82f4a59334f4ea80
Nirmal Senthilkumar <senthi22@purdue.edu>
2023-11-12 20:36:48 -0500

    revamped Product class

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * Product class:
  * creates a new product object
@@ -8,11 +11,12 @@
  */
 
 public class Product {
-    private static int ProductIDCounter;
+    private static int ProductIDCounter = 0;
     private String name;
     private double price;
     private String description;
     private int ProductID;
+    private static ArrayList<Product> products;
 
     // store associated with the product
 
@@ -22,6 +26,50 @@ public class Product {
         this.description = productDescription;
         this.ProductID = ProductIDCounter;
         ProductIDCounter++;
+    }
+
+    public Product(int productID, String name, double price, String productDescription) {
+        this.name = name;
+        this.price = price;
+        this.description = productDescription;
+        this.ProductID = productID;
+    }
+
+    public void populateProductList() {
+        File productFile = new File("products.txt");
+        try {
+            FileReader fr = new FileReader(productFile);
+            BufferedReader bfr = new BufferedReader(fr);
+            String line = bfr.readLine();
+            products = new ArrayList<>();
+            while (line != null) {
+                String[] productDetail = line.substring(8, line.length() - 1).split(";");
+                products.add(new Product(Integer.parseInt(productDetail[0]), productDetail[1],
+                        Double.parseDouble(productDetail[2]), productDetail[3]));
+                line = bfr.readLine();
+            }
+        } catch (IOException ignored) {
+        }
+    }
+
+    public void saveProduct(Product product) {
+        if (products.contains(product))
+            return;
+        try {
+            File productFile = new File("products.txt");
+            FileWriter fw = new FileWriter(productFile, false);
+            BufferedWriter bfw = new BufferedWriter(fw);
+            bfw.write(product.toString());
+        } catch (IOException ignored) {
+        }
+    }
+
+    public Product getProduct(int productID) {
+        for (Product product : products) {
+            if (product.getProductID() == productID)
+                return product;
+        }
+        return null;
     }
 
     public String getDescription() {
@@ -73,6 +121,6 @@ public class Product {
     }
 
     public String toString() {
-        return "Product<" + getName() + ";" + getPrice() + ";" + getProductDescription() + ">";
+        return "Product<" + getProductID() + ";" + getName() + ";" + getPrice() + ";" + getProductDescription() + ">";
     }
 }
