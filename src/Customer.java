156d7d1c891756f50b1d7910a42621e3570896c8
Nirmal Senthilkumar <senthi22@purdue.edu>
2023-11-13 05:28:40 -0500

    new constructors/getters/setters

--- src/Customer.java
+++ src/Customer.java
@@ -3,11 +3,6 @@
  * <p>
  * Initializes a customer object and
  * manages their buying history and identification information
- * Customer info is saved in the following format:
- * First Line: email Name
- * Second Line to productLength+1 Line: Product.toString() + quantity;
- * productLength+2 Line: Shopping Cart:
- * ProductLength+3 - productLength+3+ShoppingCartLength: Product.toString() + quantity;
  *
  * @author Nirmal Senthilkumar, Ankita Majumdar, Lalitha Chandolu; CS 180 Black
  * @version November 11, 2023
@@ -16,4 +11,34 @@ public class Customer {
     private String name;
     private String email;
     private String password;
+
+    public Customer(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
