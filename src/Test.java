2bcc6b865cd41e4260620775abb4477d76805601
Lalitha Chandolu <143627196+lalitha-chandolu@users.noreply.github.com>
2023-11-13 20:59:20 -0500

    Create Test.java

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+  /** Test Cases File
+  * Uses extensive test cases to test the functionality of all of our methods in the project
+  * And to verify that the user workflow is as expected for a Seller vs for a Buyer
+  *
+  * @author 
+  * @version November 14, 2023
+
+  public static void main(String[] args) {
+    // call each test case method from here
+
+  }  
+
+
+
+
+}
